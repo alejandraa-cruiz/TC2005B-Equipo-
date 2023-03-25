ee1425468459161e25b8dd9c54cca3e26c70ebcd
Dani Gutiérrez <a01068056@tec.mx>
2023-03-25 01:10:13 -0600

    Add teamMember model; login and logout routes

--- ProjectNexus/src/routes/home.routes.js
+++ ProjectNexus/src/routes/home.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { auth, requiresAuth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const homeController = require('../controllers/home.controller');
 
 let router = express.Router();
