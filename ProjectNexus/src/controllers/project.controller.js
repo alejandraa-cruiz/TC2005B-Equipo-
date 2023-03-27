14b533b82f06b752a0756c76c0805676f787b93e
Dani Gutiérrez <a01068056@tec.mx>
2023-03-27 14:24:15 -0600

    feat: added project's model and controller

---
+++ ProjectNexus/src/controllers/project.controller.js
@@ -0,0 +1,32 @@
+const Project = require('../models/project.model');
+
+exports.postProject = (req, res) => {
+    const requestProject = req.body;
+    Project.fetch_name(requestProject.project_name)
+    .then(([rows, fiedlData]) =>{
+        // If project not in database
+        if(rows === 0){
+            const newProject = new project ({
+                project_name: data.project_name,
+                start_date: data.start_date,
+                end_date: data.end_date
+            });
+            newProject.save()
+            .then(([rows, fieldData]) =>{
+                res.redirect('/dashboard');
+            })
+            // Prompt message of project already in
+            // database
+        } else{
+            // TODO: fetch userInfo and then send query with
+            // username
+            const userInfo = req.oidc.fetchUserInfo();
+            let query = encodeURIcomponent(userInfo.name);
+            res.redirect('/createProject?error=' + query);
+        }
+    })
+    // Create view of not inputs inserted
+    // .catch(error => {
+    //     res.redirect('');
+    // })
+}
--- ProjectNexus/src/routes/home.routes.js
+++ ProjectNexus/src/routes/home.routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { requiresAuth } = require('express-openid-connect');
 const homeController = require('../controllers/home.controller');
 const uploadController = require('../controllers/upload.contoller');
+const projectController = require('../controllers/project.controller');
 
 let router = express.Router();
 
 router.get('/', requiresAuth(), homeController.dashboard);
 router.post('/upload', requiresAuth(), uploadController.file);
+router.get('/createProject', requiresAuth(), projectController.postProject);
 
 module.exports = router;
