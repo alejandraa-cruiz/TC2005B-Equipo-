b0d9e6127215a9cda7844390ea12bf197e584bdd
A01068056_DanyGuti <105307817+DanyGuti@users.noreply.github.com>
2023-03-28 21:10:34 -0600

    Revert "dany/auth"

--- ProjectNexus/src/controllers/home.controller.js
+++
@@ -1,8 +0,0 @@
-exports.dashboard = async (req, res) => {
-    try {
-        const userInfo = await req.oidc.fetchUserInfo();
-        res.render(__dirname + '/../views/home', { user: userInfo });
-    } catch {
-        res.redirect('/logout');
-    }
-}
--- ProjectNexus/src/routes/project.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { requiresAuth } = require('express-openid-connect');
-const ProjectController = require('../controllers/project.controller');
-
-let router = express.Router();
-
-router.get('/', requiresAuth(), ProjectController.postProject);
-
-module.exports = router;
