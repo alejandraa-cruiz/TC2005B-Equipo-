8fd7fe2f975776cd5f25b1221b1cfae6aeadedb3
Peblo <a01275676@tec.mx>
2023-04-10 02:08:11 -0600

    feat: added file model

---
+++ ProjectNexus/src/models/csv.model.js
@@ -0,0 +1,14 @@
+const db = require("../utils/database");
+
+module.exports = class Csv {
+    constructor(csv) {
+        this.id_team_member = csv.id_team_member || null;
+        this.id_sprint = csv.id_sprint || null;
+        this.file_path = csv.file_path;
+    }
+
+    save () {
+        return db.execute('INSERT INTO csv (file_path) VALUES (?)', 
+                           [this.file_path]);
+    }
+}
