7a2fd141608a0d3f0d53277017678ed827520d7d
KARLA PADILLA <A01705331@tec.mx>
2023-04-11 22:57:00 -0600

    Feat: added project alerts

---
+++ ProjectNexus/src/public/scripts/project.js
@@ -0,0 +1,42 @@
+const alertProject = document.getElementById("alert");
+const alertSuccProjectErrors = document.getElementById("alertSucc");
+const messaggeProjectErrors = document.getElementById("message-error");
+const messaggeSuccessProjectErrors = document.getElementById("message-success");
+
+
+function handleSubmitProject (){
+    const projectForm = document.getElementById("form-postproject");
+    const data = new FormData(projectForm);
+    fetch(`/project/create`,{
+        method: "POST",
+        body : data
+    })
+    .then(res => {
+        return(res.json());
+    })
+    .then(res => {
+        let messages = res.e;
+
+        if (messages === 'Success'){
+            messaggeSuccessProjectErrors.innerText = 'Success';
+            alertSuccProjectErrors.classList.remove("hidden");
+            setTimeout(function () {alertSuccProjectErrors.classList.add('hidden')}, 3000);
+        }
+        else if (messages === 'Project already exists'){
+            messaggeProjectErrors.innerText = 'Project already exists';
+            alertProject .classList.remove("hidden");
+            setTimeout(function () {alertProject.classList.add('hidden')}, 3000);
+        }
+        else if (messages === 'Invalid time range'){
+            messaggeProjectErrors.innerText = 'Invalid time range';
+            alertProject.classList.remove("hidden");
+            setTimeout(function () {alertProject.classList.add('hidden')}, 3000);
+        }
+        else if (messages === 'Database conection failed'){
+            messaggeProjectErrors.innerText = 'Database conection failed';
+            alertProject.classList.remove("hidden");
+            setTimeout(function () {alertProject.classList.add('hidden')}, 3000);
+        }
+        projectForm.reset();
+    })
+}
--- ProjectNexus/src/routes/project.routes.js
+++ ProjectNexus/src/routes/project.routes.js
@@ -4,12 +4,11 @@ const ProjectController = require('../controllers/project.controller');
 
 let router = express.Router();
 
+router.get('/epics',requiresAuth(), ProjectController.getEpicsProjects);
 router.get('/', requiresAuth(), ProjectController.project);
 router.post('/create', requiresAuth(), ProjectController.postProject);
 router.get('/list', requiresAuth(), ProjectController.getListProjects);
 router.get('/list/:query', requiresAuth(), ProjectController.getListProjectsSearchBar);
 router.delete('/delete/:project', requiresAuth(), ProjectController.deleteProject);
-
-// router.post('/list', ProjectController.postListProjects);
 
 module.exports = router;
